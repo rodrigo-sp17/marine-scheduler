98782c922562c2fa2333ba3428f4c4fdcf4d300f
Rodrigo Silva <rodrigosilvap93@gmail.com>
2021-02-26 21:11:46 -0300

    Add CORS for heroku

--- src/main/java/com/github/rodrigo_sp17/mscheduler/security/SecurityConfig.java
+++ src/main/java/com/github/rodrigo_sp17/mscheduler/security/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -78,11 +83,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // DelegatingPasswordEncoder is used to allow multiple algorithms and be ready to change
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-/*    @Bean
+
+    @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods();
-    }*/
+        configuration.setAllowedOrigins(Arrays.asList("https://agendamaritima.herokuapp.com/"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers",
+                "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
